79c62536454bfca6bca5c821455bb5189c658a86
Leonardo Tavares <leo.tav.12345@gmail.com>
2025-09-03 02:47:26 -0300

    feat: finish

--- script.js
+++ script.js
@@ -1,37 +1,81 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const images = document.querySelectorAll(".image-container img");
+  const images = document.querySelectorAll(".image-container");
   const largeImage = document.querySelector(".container-large-image img");
   const containerLarge = document.querySelector(".container-large-image");
+  const gridItems = document.querySelectorAll(".card-item");
 
   addListenerToImages();
   addListenerToContainer();
-
-  //   function populateItemViewTransitionName() {
-  //     const items = document.querySelectorAll(".image-container img");
-  //     items.forEach((item) => {
-  //       item.style.viewTransitionName = `item-${item.dataset.id}`;
-  //     });
-  //   }
+  populateGridItemsViewTransitionName();
 
   function addListenerToImages() {
     for (const image of images) {
-      image.addEventListener("click", () => {
-        document.body.style.overflow = "hidden";
+      image.addEventListener("click", async () => {
+        const img = image.querySelector("img");
 
-        document.startViewTransition(() => {
-          largeImage.src = image.src;
+        img.classList.add("active");
+
+        img.style.viewTransitionName = "image";
+        largeImage.style.viewTransitionName = "none";
+
+        if (!document.startViewTransition) {
+          containerLarge.classList.add("active");
+          largeImage.src = img.src;
+          return;
+        }
+
+        const transition = document.startViewTransition(() => {
+          largeImage.src = img.src;
+          img.style.viewTransitionName = "none";
+          largeImage.style.viewTransitionName = "image";
           containerLarge.classList.add("active");
         });
+
+        await transition.finished;
+
+        image.style.viewTransitionName = "none";
+        largeImage.style.viewTransitionName = "none";
       });
     }
   }
 
   function addListenerToContainer() {
-    containerLarge.addEventListener("click", () => {
+    containerLarge.addEventListener("click", async () => {
+      const activeImage = document.querySelector(".active img");
       document.body.style.overflow = "auto";
 
-      document.startViewTransition(() => {
+      if (!document.startViewTransition) {
         containerLarge.classList.remove("active");
+        activeImage.classList.remove("active");
+        return;
+      }
+
+      const transition = document.startViewTransition(() => {
+        activeImage.style.viewTransitionName = "image";
+        largeImage.style.viewTransitionName = "none";
+        containerLarge.classList.remove("active");
+        activeImage.classList.remove("active");
+      });
+
+      await transition.finished;
+
+      activeImage.style.viewTransitionName = "none";
+      largeImage.style.viewTransitionName = "none";
+    });
+  }
+
+  function populateGridItemsViewTransitionName() {
+    if (!document.startViewTransition) {
+      return;
+    }
+
+    gridItems.forEach((item, index) => {
+      item.style.viewTransitionName = `card-item-${index}`;
+
+      item.addEventListener("click", () => {
+        document.startViewTransition(() => {
+          item.remove();
+        });
       });
     });
   }
